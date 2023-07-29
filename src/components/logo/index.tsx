378a3ab28949cc7e813a18c081db43a16f7dad00
Justin Kim <justinkim943@gmail.com>
2023-07-29 12:58:24 -0700

    added light mode. desktop home page done

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -2,11 +2,16 @@ import { PageRoute } from "@/constants/page-route";
 import styles from "./logo.module.scss";
 import clsx from "clsx";
 import { HeaderLink } from "@/components/header/header-link";
+import { useColorContext } from "@/context/color-context";
 
 export function Logo() {
+  const { isLightMode } = useColorContext();
+
   return (
     <HeaderLink
-      className={clsx(styles.logo, "fancy-link")}
+      className={clsx(styles.logo, "fancy-link", {
+        [styles["logo--light-mode"]]: isLightMode,
+      })}
       href={PageRoute.home}
     >
       <span className={styles["logo-thin"]}>the</span>acdx
